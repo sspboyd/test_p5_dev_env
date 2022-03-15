a37acc49b745d802dee2731bc301590f9a2e2b81
sspboyd <stephen@sspboyd.ca>
2022-03-15 09:46:40 -0400

    Minor change to test graphic and eslint comment.

--- src/app.js
+++ src/app.js
@@ -5,8 +5,9 @@ const script = (p55) => {
   };
   p55.draw = () => {
     p55.clear();
-    p55.ellipse(p55.mouseX, p55.mouseY, 420, 220);
+    p55.ellipse(p55.mouseX, p55.mouseY, 76, 123);
   };
 };
 
+// eslint-disable-next-line no-unused-vars
 const myp5 = new P5(script, "p55_div");
